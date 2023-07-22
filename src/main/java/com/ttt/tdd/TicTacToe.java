d75340188089a1f352df3d0c6afdeb351f0cb1ab
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-23 01:00:31 +0530

    Add: Check if its occupied

--- src/main/java/com/ttt/tdd/TicTacToe.java
+++ src/main/java/com/ttt/tdd/TicTacToe.java
@@ -1,7 +1,14 @@
 package com.ttt.tdd;
 
 public class TicTacToe {
-	
+
 	private Character[][] board = { { '\0', '\0', '\0' }, { '\0', '\0', '\0' }, { '\0', '\0', '\0' } };
+
+	public void play(int column, int row) {
+
+		if (board[column - 1][row - 1] != '\0') {
+			throw new RuntimeException("Field is occupied!");
+		}
+	}
 
 }
--- src/test/java/com/ttt/tdd/TicTacToeTest.java
+++ src/test/java/com/ttt/tdd/TicTacToeTest.java
@@ -2,13 +2,25 @@ package com.ttt.tdd;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 public class TicTacToeTest {
 	
 	TicTacToe ticTacToe = new TicTacToe();
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
 	@Test
 	public void initializeNewTicTacToeGame() {
 		assertNotNull(ticTacToe);
 	}
+	
+	@Test
+    public void whenOccupiedThenRuntimeException() {
+        ticTacToe.play(2,1);
+        exception.expect(RuntimeException.class);
+    }
 }
