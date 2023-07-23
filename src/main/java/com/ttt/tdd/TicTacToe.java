1e3ddbe53f37d289a6ce2973373962eb5fda46a6
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-23 23:29:49 +0530

    Refractor: refactor play method().

--- src/main/java/com/ttt/tdd/TicTacToe.java
+++ src/main/java/com/ttt/tdd/TicTacToe.java
@@ -12,20 +12,19 @@ public class TicTacToe {
 		checkAxis(row, "Y value is outside the board!");
        lastPlayer = nextPlayer();
        setField(column, row,"Field is occupied!",lastPlayer);
-       return checkWin();
-
+       if (isWinner()) {
+           return lastPlayer + " is the Winner";
+       }
+       return "No winner";
 	}
 	
-	private String checkWin() {
-        String winner = "No winner";
-        for (int index = 0; index < 3; index++) {
-            if (board[0][index] == lastPlayer &&
-                board[1][index] == lastPlayer &&
-                board[2][index] == lastPlayer) {
-                return lastPlayer + " is the Winner";
+	private boolean isWinner() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == lastPlayer && board[1][i] == lastPlayer &&  board[2][i] == lastPlayer) {
+                return true;
             }
         }
-        return winner;
+        return false;
     }
 
 	private void setField(int column, int row,String msg,char lastPlayer) {
