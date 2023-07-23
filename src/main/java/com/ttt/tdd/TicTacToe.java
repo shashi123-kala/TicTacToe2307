2d0c45decd925b4ce01512242037bde4414d0e8b
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-07-24 00:45:57 +0530

    Add: If its draw

--- src/main/java/com/ttt/tdd/TicTacToe.java
+++ src/main/java/com/ttt/tdd/TicTacToe.java
@@ -10,36 +10,39 @@ public class TicTacToe {
 	public String play(int column, int row) {
 		checkAxis(column, "X value is outside the board!");
 		checkAxis(row, "Y value is outside the board!");
-       lastPlayer = nextPlayer();
-       setField(column, row,"Field is occupied!",lastPlayer);
-       if (isWinner()) {
-           return lastPlayer + " is the Winner";
-       }
-       return "No winner";
+		lastPlayer = nextPlayer();
+		setField(column, row, "Field is occupied!", lastPlayer);
+		if (isWinner()) {
+			return lastPlayer + " is the Winner";
+		}
+		else if (isDraw()) {
+            return "It is a draw";
+        } 
+		else{
+			return "No winner";
+					}
 	}
-	
-	private boolean isWinner() {
-        for (int i = 0; i < 3; i++) {
-            if (board[0][i] == lastPlayer && board[1][i] == lastPlayer &&  board[2][i] == lastPlayer) {
-                return true;
-            }
-            else if (board[i][0] == lastPlayer && board[i][1] == lastPlayer &&  board[i][2] == lastPlayer) {
-                return true;
-            }
-            else if (board[0][0] == lastPlayer &&  board[1][1] == lastPlayer && board[2][2] == lastPlayer) 
-            return true;
-            else if(board[2][0] ==lastPlayer && board[1][1] ==lastPlayer && board[0][2] == lastPlayer)
-            	return true;
-        }
-        return false;
-    }
 
-	private void setField(int column, int row,String msg,char lastPlayer) {
+	private boolean isWinner() {
+		for (int i = 0; i < 3; i++) {
+			if (board[0][i] == lastPlayer && board[1][i] == lastPlayer && board[2][i] == lastPlayer) {
+				return true;
+			} else if (board[i][0] == lastPlayer && board[i][1] == lastPlayer && board[i][2] == lastPlayer) {
+				return true;
+			} else if (board[0][0] == lastPlayer && board[1][1] == lastPlayer && board[2][2] == lastPlayer)
+				return true;
+			else if (board[2][0] == lastPlayer && board[1][1] == lastPlayer && board[0][2] == lastPlayer)
+				return true;
+		}
+		return false;
+	}
+
+	private void setField(int column, int row, String msg, char lastPlayer) {
 		if (board[column - 1][row - 1] != '\0') {
-            throw new TicTacToeException(msg);
-        } else {
-            board[column - 1][row - 1] = lastPlayer;
-        }		
+			throw new TicTacToeException(msg);
+		} else {
+			board[column - 1][row - 1] = lastPlayer;
+		}
 	}
 
 	public char nextPlayer() {
@@ -55,4 +58,15 @@ public class TicTacToe {
 			throw new TicTacToeException(message);
 		}
 	}
+	
+	private boolean isDraw() {
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                if (board[x][y] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
